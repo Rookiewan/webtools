0147b76199a12cc4b349511b286502ce1859167d
Rookiewan <939926954@qq.com>
2017-12-10 03:02:34 +0800

    rename to tools

--- upx.js
+++ upx.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs')
 
 const UPX_EXE = 'D:/Software/upx394w/upx.exe'
-const APP_PATH = './dist/heartBeat-win32-x64'
+const APP_PATH = './dist/tools-win32-x64'
 
 shell.exec(`${UPX_EXE} ${APP_PATH}/*.exe`)
 shell.exec(`${UPX_EXE} ${APP_PATH}/node.dll`)
