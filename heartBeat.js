ab617b1809d52758c6b9faf792d749d5b3888691
Rookiewan <939926954@qq.com>
2017-12-10 03:00:31 +0800

    完成首页和心跳页面

--- heartBeat.js
+++ heartBeat.js
@@ -1,160 +1,216 @@
-
 const request = require('superagent')
 const fs = require('fs')
 const path = require('path')
 const asyncLib = require('async')
 
-// cookies 列表
-let cookies = []
-
-// cookies 文件名
-const cookiesFilename = 'cookies.txt'
-// 投票页面 url
-const URL = 'http://www.zghyyw.com/index.php?m=Termin&c=VoteGoodActor&a=vote&opid=6ff61f410886e10d33ec96ae02c6deef'
-// 投票的请求 url
-const VOTE_URL = 'http://www.zghyyw.com/index.php?m=Termin&c=VoteGoodActor&a=do_vote'
-
-// test
-const My_COOKIE = 'PHPSESSID=a1m50gpb3l8v20evos1l3e5mv3'
-const EXPIRE_COOKIE = 'PHPSESSID=1i3tqgmh7s8392sdd1lhmt3g24'
-
-// 计时器
-let counter = null
-// 一分钟
-const ONE_MINUTE = 1000 * 60
-// 心跳间隔
-const BEAT_INTERVAL = ONE_MINUTE * 5
-// 每次心跳最大并发数
-const LIMIT = 10
-// 本次心跳循环次数
-let beatCount = 0
-
-readCookies(cookiesFilename)
-let totalCount = cookies.length
-// doVote()
-// doHeartBeat(EXPIRE_COOKIE)
-startHeartBeat()
-
-function startHeartBeat () {
-  const _LIMIT = cookies.length >= LIMIT ? LIMIT : cookies.length
-  if (cookies.length > 0) {
-    asyncLib.mapLimit(cookies, _LIMIT, (cookie, callback) => {
-      // if (!cookie.active) {
-      //   callback(null)
-      //   return
-      // }
-      doHeartBeat(cookie.cookie)
-        .then(() => {
-          cookie.active = true
-          callback(null)
-        })
-        .catch(err => {
-          cookie.active = false
-          callback(null)
-        })
-    }, (err) => {
-      if (err) {
-        throw err
-      }
-      // removeCookies()
-      let successCount = cookies.filter(_ => _.active === true).length
-      console.log('\n')
-      console.log('心跳间隔: ' + (BEAT_INTERVAL / 1000) + '秒')
-      console.log('总数量: ' + totalCount)
-      console.log('心跳成功数量: ' + successCount)
-      console.log('心跳次数: ' + (++beatCount))
-      console.log('\n\n')
-      counter = setTimeout(startHeartBeat, BEAT_INTERVAL)
-    })
-  } else {
-    clearTimeout(counter)
+class HeartBeat {
+  constructor (params = {}) {
+    const ONE_MINUTE = 1000 * 60
+    const VOTE_URL = 'http://www.zghyyw.com/index.php?m=Termin&c=VoteGoodActor&a=do_vote'
+    const PAGE_URL = 'http://www.zghyyw.com/index.php?m=Termin&c=VoteGoodActor&a=vote&opid=6ff61f410886e10d33ec96ae02c6deef'
+    const BEAT_INTERVAL = ONE_MINUTE * 5
+    const LIMIT = 10
+    const CONFIG = {
+      cookieFilePath: 'cookies.txt',
+      url: PAGE_URL,
+      interval: BEAT_INTERVAL,
+      limit: LIMIT,
+      successFlag: '中国电视好演员投票'
+    }
+    this.config = Object.assign({}, CONFIG, params)
+    this.cookies = []
+    this.counter = null
+    this.beatCount = 0
+    this.totalCount = 0
+    this.successCount = 0
+    this.looping = false
+    this.beatCallback = () => {}
+    this.cookieBeatBack = () => {}
+    this.readCookies(this.config.cookieFilePath)
   }
-}
-
-// 投票
-function doVote (cookie) {
-  request
-    .post(VOTE_URL)
-    .set({
-      'Host': 'www.zghyyw.com',
-      'Connection': 'keep-alive',
-      // 'Content-Length': 148,
-      'Accept': '*/*',
-      'Origin': 'http://www.zghyyw.com',
-      'X-Requested-With': 'XMLHttpRequest',
-      'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Le X820 Build/FEXCNFN5902605092S; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043632 Safari/537.36 MicroMessenger/6.5.19.1140 NetType/WIFI Language/zh_CN',
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Referer': 'http://www.zghyyw.com/index.php?m=Termin&c=VoteGoodActor&a=vote&opid=3fe339e16bab15d6f6103043b7f4d6e2',
-      'Accept-Encoding': 'gzip, deflate',
-      'Accept-Language': 'zh-CN,en-US;q=0.8',
-      'Cookie': cookie
+  // 读取cookies
+  readCookies (cookiesFileName) {
+    let filePath = cookiesFileName
+    if (!/(\/|\\)/.test(cookiesFileName)) {
+      // 传入的是文件名
+      filePath = path.resolve(__dirname,cookiesFileName)
+    }
+    let cookiesStr = fs.readFileSync(filePath).toString()
+    cookiesStr.split('\r\n').map(_ => {
+      try {
+        let cookie = _.split(';')[0]
+        if (cookie) {
+          this.cookies.push({
+            active: true,
+            cookie
+          })
+        }
+        this.totalCount = this.cookies.length
+      } catch (err) {}
     })
-    .end((err, res) => {
-      if (err) {
-        throw err
-      }
-      console.log(res.text)
-    })
-}
-
-// 心跳
-function doHeartBeat (cookie) {
-  return new Promise((resolve, reject) => {
-    request
-      .get(URL)
-      .set({
-        'Host': 'www.zghyyw.com',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': 1,
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/wxpic,image/sharpp,*/*;q=0.8',
-        'Referer': 'http://www.zghyyw.com/index.php?m=Termin&c=VoteGoodActor&a=index&from=singlemessage',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/wxpic,image/sharpp,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,en-US;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Le X820 Build/FEXCNFN5902605092S; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043632 Safari/537.36 MicroMessenger/6.5.19.1140 NetType/WIFI Language/zh_CN',
-        'Cookie': cookie
-      })
-      .end((err, res) => {
+  }
+  startHeartBeat (handMode) {
+    this.looping = true
+    const _LIMIT = this.cookies.length >= this.config.limit ? this.config.limit : this.cookies.length
+    if (this.cookies.length > 0) {
+      asyncLib.mapLimit(this.cookies, _LIMIT, (cookie, callback) => {
+        if (!this.looping) {
+          callback(null)
+          return
+        }
+        // if (!cookie.active) {
+        //   callback(null)
+        //   return
+        // }
+        this.doHeartBeat(cookie.cookie)
+          .then(() => {
+            cookie.active = true
+            try {
+              this.cookieBeatBack(cookie)
+            } catch (err) {}
+            callback(null)
+          })
+          .catch(err => {
+            cookie.active = false
+            try {
+              this.cookieBeatBack(cookie)
+            } catch (err) {}
+            callback(null)
+          })
+      }, (err) => {
         if (err) {
           // throw err
           console.log(err)
         }
-        const regx = /中国电视好演员投票/
-        if (regx.test(res.text)) {
-          // 心跳成功
-          console.log(cookie + ' >>> 心跳成功')
-          resolve()
-        } else{
-          console.log(cookie + ' >>> 心跳失败')
-          reject()
+        // removeCookies()
+        this.successCount = this.cookies.filter(_ => _.active === true).length
+        // console.log('\n')
+        // console.log('心跳间隔: ' + (this.config.interval / 1000) + '秒')
+        // console.log('总数量: ' + this.totalCount)
+        // console.log('心跳成功数量: ' + this.successCount)
+        // console.log('心跳次数: ' + (++this.beatCount))
+        // console.log('\n\n')
+        try {
+          this.beatCallback({
+            beatCount: this.beatCount,
+            totalCount: this.totalCount,
+            successCount: this.successCount,
+            looping: this.looping
+          })
+        } catch (err) {}
+        if (!handMode) {
+          this.counter = setTimeout(() => {
+            this.startHeartBeat()
+          }, this.config.interval)
         }
       })
-  })
-}
-
-// 每次心跳后更新cookies列表
-function removeCookies () {
-  let newCookies = []
-  cookies.map((_, i) => {
-    if (_.active) {
-      newCookies.push(_)
+    } else {
+      this.stop()
     }
-  })
-  cookies = newCookies
+  }
+  // 心跳
+  doHeartBeat (cookie) {
+    return new Promise((resolve, reject) => {
+      request
+        .get(this.config.url)
+        .set({
+          'Host': 'www.zghyyw.com',
+          'Connection': 'keep-alive',
+          'Upgrade-Insecure-Requests': 1,
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/wxpic,image/sharpp,*/*;q=0.8',
+          'Referer': 'http://www.zghyyw.com/index.php?m=Termin&c=VoteGoodActor&a=index&from=singlemessage',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/wxpic,image/sharpp,*/*;q=0.8',
+          'Accept-Encoding': 'gzip, deflate',
+          'Accept-Language': 'zh-CN,en-US;q=0.8',
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Le X820 Build/FEXCNFN5902605092S; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043632 Safari/537.36 MicroMessenger/6.5.19.1140 NetType/WIFI Language/zh_CN',
+          'Cookie': cookie
+        })
+        .end((err, res) => {
+          if (err) {
+            // throw err
+            console.log(err)
+            reject()
+          }
+          const regx = new RegExp(this.config.successFlag)
+          if (regx.test(res.text)) {
+            // 心跳成功
+            // console.log(cookie + ' >>> 心跳成功')
+            resolve()
+          } else{
+            // console.log(cookie + ' >>> 心跳失败')
+            reject()
+          }
+        })
+    })
+  }
+  // 每次心跳后更新cookies列表
+  removeCookies () {
+    let newCookies = []
+    this.cookies.map((_, i) => {
+      if (_.active) {
+        newCookies.push(_)
+      }
+    })
+    this.cookies = newCookies
+  }
+  // 投票
+  doVote (cookie) {
+    request
+      .post(this.config.voteUrl)
+      .set({
+        'Host': 'www.zghyyw.com',
+        'Connection': 'keep-alive',
+        // 'Content-Length': 148,
+        'Accept': '*/*',
+        'Origin': 'http://www.zghyyw.com',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Le X820 Build/FEXCNFN5902605092S; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043632 Safari/537.36 MicroMessenger/6.5.19.1140 NetType/WIFI Language/zh_CN',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Referer': 'http://www.zghyyw.com/index.php?m=Termin&c=VoteGoodActor&a=vote&opid=3fe339e16bab15d6f6103043b7f4d6e2',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,en-US;q=0.8',
+        'Cookie': cookie
+      })
+      .end((err, res) => {
+        if (err) {
+          throw err
+        }
+        console.log(res.text)
+      })
+  }
+  // 停止心跳
+  stop () {
+    try {
+      this.beatCallback({
+        beatCount: this.beatCount,
+        totalCount: this.totalCount,
+        successCount: this.successCount,
+        looping: this.looping
+      })
+    } catch (err) {}
+    clearTimeout(this.counter)
+    this.totalCount = 0
+    this.beatCount = 0
+    this.cookies = []
+    this.looping = false
+  }
+  // 每次更新完成 回调
+  onBeatBack (callback) {
+    this.beatCallback = callback
+  }
+  // 每个cookie心跳回调
+  onCookieBeatBack (callback) {
+    this.cookieBeatBack = callback
+  }
+  // 手动心跳
+  beatByHand () {
+    this.startHeartBeat(true)
+  }
 }
 
-// 读取cookies
-function readCookies (cookiesFileName) {
-  let cookiesStr = fs.readFileSync(path.resolve(__dirname, cookiesFileName)).toString()
-  cookiesStr.split('\r\n').map(_ => {
-    try {
-      let cookie = _.split(';')[0]
-      if (cookie) {
-        cookies.push({
-          active: true,
-          cookie
-        })
-      }
-    } catch (err) {}
-  })
-}
+module.exports = HeartBeat
+
+// let heartBeat = new HeartBeat({
+  // interval: 10000
+// })
+// heartBeat.startHeartBeat()
--- main.js
+++ main.js
@@ -1,38 +1,59 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, Tray } = require('electron')
 const path = require('path')
 const url = require('url')
-const request = require('superagent')
+const HeartBeat = require('./heartBeat.js')
 
-let win
+let mainWin
+let winsCount = 0
+let wins = {}
+let tray = null
 
-function createWindow () {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600
+function createWindow ({ page = 'index.html', width = 800, height = 600 }) {
+  let win = new BrowserWindow({
+    width,
+    height,
+    icon: __dirname + '/assets/icon.png'
   })
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, page),
     protocol: 'file:',
     slashes: true
   }))
 
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
+  wins[win.id] = {
+    win: win,
+    instance: null
+  }
+  // win.webContents.executeJavaScript(`window.WIN_ID='${id}'`)
 
-  request
-    .get('https://www.baidu.com')
-    .end((err, res) => {
-      if (err) {
-        throw err
-      }
-      console.log(res.text)
-    })
-
-  win.on('closed', () => {
-    win = null
+  // 即将被关闭
+  win.on('close', e => {
+    win.webContents.send('onClose')
+    e.preventDefault()
   })
+  // 被关闭后释放资源
+  win.on('closed', e => {
+    console.log(win.id + ' has closed')
+    try {
+      wins[win.id].instance.stop()
+    } catch (err) {}
+    delete wins[win.id]
+  })
+
+  return win
 }
 
-app.on('ready', createWindow)
+app.on('ready', () => {
+  mainWin = createWindow({})
+  tray = new Tray('./assets/icon.png')
+  tray.setToolTip('tools，双击显示')
+  tray.on('double-click', () => {
+    Object.keys(wins).map(winId => {
+      wins[winId].win.show()
+    })
+  })
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -42,6 +63,65 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (win === null) {
-    createWindow()
+    mainWin = createWindow()
   }
+})
+
+// 打开新页面
+ipcMain.on('open-page', (event, page) => {
+  let pageUrl = 'index.html'
+  let width = 800
+  let height = 600
+  switch (page) {
+    case 'heartBeat':
+      pageUrl = 'heart-beat.html'
+      width = 800
+      height = 580
+      break
+    default:
+  }
+  let _win = createWindow({
+    page: pageUrl,
+    width,
+    height
+  })
+})
+
+// 心跳服务
+ipcMain.on('heart-beat', (event, { option, params }) => {
+  let winId = event.sender.id
+  switch (option) {
+    case 'doBeat':
+      // opt: 1 启动, 2 停止, 3手动心跳
+      let { opt, options } = params
+      if (opt === 1) {
+        let heartBeat = new HeartBeat(options)
+        wins[winId].instance = heartBeat
+        event.sender.send('doBeat', JSON.parse(JSON.stringify(wins[winId].instance)))
+        heartBeat.startHeartBeat()
+        heartBeat.onBeatBack(instance => {
+          event.sender.send('doBeat', instance)
+        })
+        heartBeat.onCookieBeatBack(cookie => {
+          event.sender.send('doBeatCookie', cookie)
+        })
+      } else if (opt === 2) {
+        wins[winId].instance.stop()
+      } else if (opt === 3) {
+        wins[winId].instance.beatByHand()
+      }
+      break
+    default:
+  }
+})
+
+ipcMain.on('closePage', (event) => {
+  let winId = event.sender.id
+  console.log('close ' + winId)
+  try {
+    wins[winId].win.destroy()
+    wins[winId].instance.stop()
+  } catch (err) {
+  }
+  delete wins[winId]
 })
